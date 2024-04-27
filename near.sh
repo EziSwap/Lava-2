a8cfabebad9a6a88989e9e31d1aa751a80ff618c
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-04-27 22:50:33 +0300

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-b8014ee7-f6be-4619-86eb-293695f42eb4/"
 
 OUTPUT_DIR="logs"
 
-near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+near_data=$(curl -A UA -x http://premium.proxywing.com:12321 --proxy-user d9hv1twu6q:rzld6kr3mb -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
         mkdir "$OUTPUT_DIR"
     fi
-    
+
     echo "$near_data" >> "$OUTPUT_DIR/near.txt"
-    
+
     echo "NEAR data fetched and saved to $OUTPUT_DIR/near.txt."
 else
     echo "Failed to fetch NEAR data."
